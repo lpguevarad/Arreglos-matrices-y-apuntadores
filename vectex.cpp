209ee0ce37bb5d383f7c8fc1819af5ab117e35c8
Leidy Guevara <lpguevarad@unal.edu.co>
2018-04-11 08:58:12 -0500

    Ejemplo vectores. Estado: incompleto

---
+++ vectex.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+double promd(const std::vector<double> & a);
+double dstand(std::vector<double> & b,const std::vector<double> & a);
+
+int main()
+{
+  int N = 10;
+  std::vector<double> x(N) {1.,2.,3.,4.,5.,6.,7.,8.,9.,10.};
+}
+
+double promd(const std::vector<double> & a)
+{
+  for (double i=0;i<N;i++){
+  double suma +=x[i];
+     }
+  std::cout << "Promedio =" << suma/N << std::endl;
+}
+
+double dstand(std::vector<double> & b,const std::vector<double> & a)
+{
+  for(int i=0;i<N;i++){
+    double dev += (x[i]*)
+  }
+}
